82577fd076d5bfcf729bd660d177c01c02b942e1
Kees Pieters <info@condast.com>
2013-09-02 13:28:54 +0200

    Fixed copyrights

--- Workspace/net.osgi.jxse.service/src/net/osgi/jxse/service/IServiceChangedListener.java
+++ Workspace/net.osgi.jxse.service/src/net/osgi/jxse/service/IServiceChangedListener.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Condast and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Kees Pieters - initial API and implementation
+ *******************************************************************************/
 package net.osgi.jxse.service;
 
 import net.osgi.jxse.utils.StringStyler;
